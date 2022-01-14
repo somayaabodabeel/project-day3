428591be3f0d4eb0844d70b65742201ecf455219
somaya_abodabeel <92024063+somayaabodabeel@users.noreply.github.com>
2022-01-15 02:39:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,13 @@
 import streamlit as st
 import joblib
 import numpy as np
+import os
 
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system(f"dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 def main():
     html_temp = """
     <div style="background-color:lightblue;padding:16px">
